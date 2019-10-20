b5b17840d40371dc6479d5af42b3df512f0d64af
huailiang <peng_huailiang@qq.com>
2019-10-20 16:25:39 +0800

    log

--- neural/util/logit.py
+++ neural/util/logit.py
@@ -5,8 +5,11 @@
 
 import os
 import logging
+import platform
+import sys
 
 """
+
 level:
     FATAL：致命错误
     CRITICAL：特别糟糕的事情，如内存耗尽、磁盘空间为空，一般很少使用
@@ -54,10 +57,15 @@ def init(name="LOG", level=logging.INFO, log_path="./output/log.txt"):
     handler.setLevel(level)
     handler.setFormatter(formatter)
     console = logging.StreamHandler()
+    formatter = logging.Formatter('%(message)s')
     console.setFormatter(formatter)
     console.setLevel(level)
     _log.addHandler(handler)
     _log.addHandler(console)
+    if platform.system() == "Windows":
+        logging.StreamHandler.emit = add_coloring_to_emit_windows(logging.StreamHandler.emit)
+    else:
+        logging.StreamHandler.emit = add_coloring_to_emit_ansi(logging.StreamHandler.emit)
 
 
 def clear_log(log_path):
@@ -65,32 +73,152 @@ def clear_log(log_path):
         if os.path.exists(log_path):
             os.remove(log_path)
     except IOError:
+        logging.error("log module error")
         raise
+
+
+def add_coloring_to_emit_windows(fn):
+    def _out_handle(self):
+        import ctypes
+        return ctypes.windll.kernel32.GetStdHandle(self.STD_OUTPUT_HANDLE)
+
+    out_handle = property(_out_handle)
+
+    def _set_color(self, code):
+        import ctypes
+        # Constants from the Windows API
+        self.STD_OUTPUT_HANDLE = -11
+        hdl = ctypes.windll.kernel32.GetStdHandle(self.STD_OUTPUT_HANDLE)
+        ctypes.windll.kernel32.SetConsoleTextAttribute(hdl, code)
+
+    setattr(logging.StreamHandler, '_set_color', _set_color)
+
+    def new(*args):
+        FOREGROUND_BLUE = 0x0001  # blue
+        FOREGROUND_GREEN = 0x0002  # green
+        FOREGROUND_RED = 0x0004  # red
+        FOREGROUND_INTENSITY = 0x0008  # text color is intensified.
+        FOREGROUND_WHITE = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED
+        # winbase.h
+        STD_INPUT_HANDLE = -10
+        STD_OUTPUT_HANDLE = -11
+        STD_ERROR_HANDLE = -12
+
+        # wincon.h
+        FOREGROUND_BLACK = 0x0000
+        FOREGROUND_BLUE = 0x0001
+        FOREGROUND_GREEN = 0x0002
+        FOREGROUND_CYAN = 0x0003
+        FOREGROUND_RED = 0x0004
+        FOREGROUND_MAGENTA = 0x0005
+        FOREGROUND_YELLOW = 0x0006
+        FOREGROUND_GREY = 0x0007
+        FOREGROUND_INTENSITY = 0x0008  # foreground color is intensified.
+
+        BACKGROUND_BLACK = 0x0000
+        BACKGROUND_BLUE = 0x0010
+        BACKGROUND_GREEN = 0x0020
+        BACKGROUND_CYAN = 0x0030
+        BACKGROUND_RED = 0x0040
+        BACKGROUND_MAGENTA = 0x0050
+        BACKGROUND_YELLOW = 0x0060
+        BACKGROUND_GREY = 0x0070
+        BACKGROUND_INTENSITY = 0x0080  # background color is intensified.
+
+        levelno = args[1].levelno
+        if levelno >= 50:
+            color = BACKGROUND_YELLOW | FOREGROUND_RED | FOREGROUND_INTENSITY | BACKGROUND_INTENSITY
+        elif levelno >= 40:
+            color = FOREGROUND_RED | FOREGROUND_INTENSITY
+        elif levelno >= 30:
+            color = FOREGROUND_YELLOW | FOREGROUND_INTENSITY
+        elif levelno >= 20:
+            color = FOREGROUND_GREEN
+        elif levelno >= 10:
+            color = FOREGROUND_MAGENTA
+        else:
+            color = FOREGROUND_WHITE
+        args[0]._set_color(color)
+
+        ret = fn(*args)
+        args[0]._set_color(FOREGROUND_WHITE)
+        return ret
+
+    return new
+
+
+def add_coloring_to_emit_ansi(fn):
+    def new(*args):
+        levelno = args[1].levelno
+        if levelno >= 50:
+            color = '\x1b[31m'  # red
+        elif levelno >= 40:
+            color = '\x1b[31m'  # red
+        elif levelno >= 30:
+            color = '\x1b[33m'  # yellow
+        elif levelno >= 20:
+            color = '\x1b[32m'  # green
+        elif levelno >= 10:
+            color = '\x1b[35m'  # pink
+        else:
+            color = '\x1b[0m'  # normal
+        if not args[1].msg.startswith(color):
+            args[1].msg = color + format_header() + args[1].msg + '\x1b[0m'
+        return fn(*args)
+
+    def format_header():
+        filename, no, function = get_stack_info()
+        pwd = os.getcwd()
+        filename = filename.replace(pwd, "")
+        return filename[1:] + ":" + str(no) + " "
+
+    def get_stack_info():
+        currentframe = lambda: sys._getframe(3)
+        f = currentframe()
+        while f is not None:
+            f = f.f_back
+            if hasattr(f, "f_code"):
+                co = f.f_code
+                if co.co_filename.find("logit.py") > 0:
+                    f = f.f_back
+                    co = f.f_code
+                    return co.co_filename, f.f_lineno, co.co_name
+            f = f.f_back
+
+    return new
 
 
 def set_level(level):
     _log.setLevel(level)
 
 
-def debug(msg, *args):
-    _log.debug(msg, *args)
+def debug(msg, *args, **kwargs):
+    _log.debug(msg, *args, **kwargs)
 
 
-def info(msg, *args):
-    _log.info(msg, *args)
+def info(msg, *args, **kwargs):
+    _log.info(msg, *args, **kwargs)
 
 
-def warn(msg, *args):
-    _log.warning(msg, *args)
+def warn(msg, *args, **kwargs):
+    _log.warning(msg, *args, **kwargs)
 
 
-def error(msg, *args):
-    _log.error(msg, *args)
+def error(msg, *args, **kwargs):
+    _log.error(msg, *args, **kwargs)
 
 
-def fatal(msg, *args):
-    _log.fatal(msg, *args)
+def fatal(msg, *args, **kwargs):
+    _log.fatal(msg, *args, **kwargs)
 
 
-def critical(msg, *args):
-    _log.critical(msg, *args)
+def critical(msg, *args, **kwargs):
+    _log.critical(msg, *args, **kwargs)
+
+
+if __name__ == '__main__':
+    init("test", level=logging.DEBUG, log_path="../output/test.txt")
+    debug("hello world {0} is {1}".format(12, "x"))
+    info("hello {0} is {1}".format((1, 2, 3), type("x")))
+    warn("hello world %s is %d", "amy", 123)
+    error("Houston, we have a %s", "major disaster", exc_info=1)
